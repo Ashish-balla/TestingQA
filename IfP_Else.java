580872945e1a6a4c83b8cf9e50b9a2c4c2ac26f3
Ashish-balla <46021054+Ashish-balla@users.noreply.github.com>
2019-07-22 14:15:22 +0530

    Add files via upload

---
+++ IfP_Else.java
@@ -0,0 +1,16 @@
+package First;
+//070719
+public class IfP_Else {
+
+	
+	public static void main(String args[]) {
+		int score =70;
+		if(score>=40) {
+			System.out.println("Pass");
+
+		}
+		else
+			System.out.println("Fail");
+	}
+
+}
